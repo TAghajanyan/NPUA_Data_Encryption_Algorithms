0c0c030a90ff1580a3a71e78fad1f812c8c15783
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-10-17 22:41:51 +0400

    Pattern Data Ok

---
+++ 001_PatternBasedMethod/001_PatternBasedMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _001_PatternBasedMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceData = null;
+
+            do
+            {
+                Console.Write("Please input data: ");
+                sourceData = Console.ReadLine();
+            } while (sourceData == null);
+
+            Transform ob = new Transform();
+
+            string encryptedString = ob.GetEncryptedString(sourceData);
+            string decryptedString = ob.GetDecryptedString(encryptedString);
+
+            Console.WriteLine($"Encrypted: {encryptedString} \nDecrypted: {decryptedString}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 001_PatternBasedMethod/001_PatternBasedMethod/Transform.cs
@@ -0,0 +1,60 @@
+﻿namespace _001_PatternBasedMethod
+{
+    class Transform
+    {
+        private string _inputPattern;
+        private string _outputPattern;
+
+        public Transform()
+        {
+            _inputPattern = "AaBbCc1234";
+            _outputPattern = "cCAaBb2143";
+        }
+
+        public string GetEncryptedString(string _sourceData)
+        {
+            string result = string.Empty;
+            int i;
+
+            foreach (char item in _sourceData)
+            {
+                for (i = 0; i < _inputPattern.Length; i++)
+                {
+                    if (item == _inputPattern[i])
+                    {
+                        result += _outputPattern[i];
+                        break;
+                    }
+                }
+
+                if (i == _inputPattern.Length)
+                    result += item;
+            }
+            return result;
+        }
+
+        public string GetDecryptedString(string _encryptedData)
+        {
+            string result = string.Empty;
+            int i;
+
+            foreach (char item in _encryptedData)
+            {
+                for (i = 0; i < _outputPattern.Length; i++)
+                {
+                    if (item == _outputPattern[i])
+                    {
+                        result += _inputPattern[i];
+                        break;
+                    }
+                }
+
+                if (i == _outputPattern.Length)
+                    result += item;
+            }
+
+            return result;
+        }
+
+    }
+}
