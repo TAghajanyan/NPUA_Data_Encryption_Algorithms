feb9e914157a702fa944d9a0b73cea3cdefbfb86
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-10-18 17:39:27 +0400

    Vigenere all okay

---
+++ 003_VigenereCodingMethod/003_VigenereCodingMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_VigenereCodingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transform transform = new Transform("LEMON");
+            string enc = transform.Encrypting("ATTACKATDAWN");
+            string dec = transform.Decrypting(enc);
+            Console.WriteLine("Encrypted text: " + enc);
+            Console.WriteLine("Decrypted text: " + dec);
+        }
+    }
+}
---
+++ 003_VigenereCodingMethod/003_VigenereCodingMethod/Transform.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _003_VigenereCodingMethod
+{
+    class Transform
+    {
+        private string _key;
+
+        public Transform(string key)
+        {
+            _key = key;
+        }
+
+        public string Encrypting(string sourceData)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string result = "";
+            int index1, index2;
+
+            for (int i = 0; i < sourceData.Length; i++)
+            {
+                if (i >= _key.Length)
+                    _key += _key;
+
+                index1 = alphabet.IndexOf(sourceData[i]);
+                index2 = alphabet.IndexOf(_key[i]);
+                result += index1 + index2 < 26 ? alphabet[index1 + index2] : alphabet[index1 + index2 - 26];
+            }
+
+            return result;
+        }
+
+        public string Decrypting(string decryptingData)
+        {
+            decryptingData = decryptingData.ToUpper();
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string result = "";
+            int index1, index2;
+
+            for (int i = 0; i < decryptingData.Length; i++)
+            {
+                if (i >= _key.Length)
+                    _key += _key;
+
+                index1 = alphabet.IndexOf(decryptingData[i]);
+                index2 = alphabet.IndexOf(_key[i]);
+                result += index1 - index2 < 0 ? alphabet[26 - Math.Abs(index1 - index2)] : alphabet[index1 - index2];
+            }
+
+            return result;
+        }
+
+    }
+}
