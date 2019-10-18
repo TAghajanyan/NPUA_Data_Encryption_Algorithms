4b063c4d92d4c7115be483e5fb133ad1c782b26c
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-10-18 15:38:57 +0400

    all ok

---
+++ 002_ColumnsAndRowsMethod/002_ColumnsAndRowsMethod/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_ColumnsAndRowsMethod
+{
+    class Transform
+    {
+        private int _size;
+        private char[,] _table;
+
+        public Transform(int size)
+        {
+            _size = size;
+        }
+
+        public string GetEncryptedText(string sourceData)
+        {
+            string encryptedString = "";
+
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = i; j < sourceData.Length; j += _size)
+                {
+                    encryptedString += sourceData[j];
+                }
+            }
+
+            //------------------------------Second Variant----------------------------
+
+            //_table = new char[sourceData.Length / _size + 1, _size];
+            //int index = 0;
+            //for (int i = 0; i < sourceData.Length / _size + 1; i++)
+            //{
+            //    for (int j = 0; j < _size && index < sourceData.Length; j++)
+            //    {
+            //        _table[i, j] = sourceData[index++];
+            //        Console.Write(_table[i, j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 0; i < _size; i++)
+            //{
+            //    for (int j = 0; j < _table.GetLongLength(0); j++)
+            //    {
+            //        encryptedString += _table[j, i];
+            //    }
+            //}
+
+            return encryptedString;
+        }
+
+        public string GetDecryptedText(string decryptingText) 
+        {
+            _table = new char[decryptingText.Length / _size + 1, _size];
+            string decryptedText = "";
+
+            int countSpaces = (int)Math.Ceiling(decryptingText.Length / (double)_size);
+
+            for (int i = 0; i < countSpaces * _size - decryptingText.Length; i++)
+            {
+                decryptingText += " ";
+            }
+
+            char[,] keyMatrix = new char[_size, countSpaces];
+
+            for (int i = 0; i < _size; i++)
+            {
+                for (int j = 0; j < countSpaces; j++)
+                {
+                    keyMatrix[i, j] = decryptingText[i * countSpaces + j];
+                }
+            }
+
+            for (int j = 0; j < countSpaces; j++)
+            {
+                for (int i = 0; i < _size; i++)
+                {
+                    decryptedText += keyMatrix[i, j];
+                }
+            }
+            return decryptedText;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transform transform = new Transform(3);
+
+            string encriptingText = transform.GetEncryptedText("12345678");
+            Console.WriteLine(encriptingText); 
+
+            Console.WriteLine(transform.GetDecryptedText(encriptingText));
+        }
+    }
+}
---
+++ 004_RLEMethod/004_RLEMethod/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_RLEMethod
+{
+
+    class Program
+    {
+        static List<string> GetEncriptedText(string sourceData)
+        {
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < sourceData.Length; i++)
+            { 
+                var count = GetCount(sourceData[i], ref sourceData, i);
+                list.Add(sourceData[i] + count.ToString());
+            }
+
+            return list;
+        }
+
+        private static int GetCount(char v, ref string sourceData, int index)
+        {
+            int count = 0;
+            for (int i = index; i < sourceData.Length; i++)
+            {
+                if (sourceData[i] == v)
+                {
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            sourceData = sourceData.Remove(index + 1, count-1);
+
+            return count;
+        }
+
+        static string GetDecriptedText(List<string> list)
+        {
+            char item;
+            int count;
+            string result = "";
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                item = list[i][0];
+                count = int.Parse(list[i].Remove(0, 1));
+                for (int j = 0; j < count; j++)
+                {
+                    result += item;
+                }
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string sourceData = "aa1111bbaa";
+            var enc = GetEncriptedText(sourceData);
+            var dec = GetDecriptedText(enc);
+
+            foreach (var item in enc)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(dec);
+
+            Console.ReadKey();
+        }
+    }
+}
