cdb6392a08ab1d17a97a4f4be3fc7e43d409cade
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-10-30 02:44:08 +0400

    Huffman is okay :D

--- HuffmanCoding/HuffmanCoding/HuffmanNodeBase.cs
+++ HuffmanCoding/HuffmanCoding/HuffmanNodeBase.cs
@@ -1,4 +1,4 @@
-﻿namespace HuffmanCod
+﻿namespace HuffmanCoding
 {
     class HuffmanNodeBase
     {
--- HuffmanCoding/HuffmanCoding/Program.cs
+++ HuffmanCoding/HuffmanCoding/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace HuffmanCod
+namespace HuffmanCoding
 {
     class Program
     {
